85e4ec821069d0373cbe82eca801843813d94276
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-08-21 19:37:15 +0530

    custom routes

---
+++ Rails_custom_routes/config/initializers/secret_token.rb
@@ -0,0 +1,26 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+SampleApp::Application.config.secret_key_base = '76f4745600a97bb235a15fc3bf7c60e3e7180d40e953506a8a89d007877ed000f5f0b8a7534cfa435a3917c4a0a1e79dbeb67a3bea6ff60e75d022b98be41a76'
